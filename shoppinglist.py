cc93e165d0315908d6655e5006e23ae1d0faa9c4
pascallaub <nutritionexpertition@gmail.com>
2024-10-02 13:46:40 +0200

    added aufgabe2

--- shoppinglist.py
+++ shoppinglist.py
@@ -4,5 +4,18 @@ def add_item():
     item = input("Bitte gib den Artikel ein, der zu Einkaufsliste hinzugefügt werden soll: ")
     list.append(item)
     print(f"{item} erfolgreich hinzugefügt!")
+    weiterer_artikel = input("Möchtest du einen weiteren Artikel eingeben? j/n: ")
+    if weiterer_artikel == 'j':
+        add_item()
+    else:
+        None
+
+def show_shoppinglist():
+    if list == []:
+        print("Noch keine Artikel hinzugefügt.")
+    else:
+        for item in list:
+            print(item)
 
 add_item()
+show_shoppinglist()
