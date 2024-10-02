abc99e06565e42b6fd0dc2253ad1040820f28dbe
pascallaub <nutritionexpertition@gmail.com>
2024-10-02 15:36:01 +0200

    added sort and delete function

--- shoppinglist.py
+++ shoppinglist.py
@@ -2,13 +2,16 @@ list = []
 
 def add_item():
     item = input("Bitte gib den Artikel ein, der zu Einkaufsliste hinzugefügt werden soll: ")
-    list.append(item)
-    print(f"{item} erfolgreich hinzugefügt!")
-    weiterer_artikel = input("Möchtest du einen weiteren Artikel eingeben? j/n: ")
-    if weiterer_artikel == 'j':
-        add_item()
-    else:
+    if item == '':
         None
+    else:
+        list.append(item)
+        print(f"{item} erfolgreich hinzugefügt!")
+        weiterer_artikel = input("Möchtest du einen weiteren Artikel eingeben? j/n: ")
+        if weiterer_artikel == 'j':
+            add_item()
+        else:
+            None
 
 def show_shoppinglist():
     if list == []:
@@ -17,13 +20,27 @@ def show_shoppinglist():
         for item in list:
             print(item)
 
+def sorted_list():
+    sortierte_liste = sorted(list)
+    for item in sortierte_liste:
+        print(item)
+
+def del_item():
+    for item in list:
+        print(item)
+    suche = input("Welchen Artikel möchtest du löschen? ")
+    list.remove(suche)
+    print("Artikel erfolgreich entfernt!")
+
 def menu():
     while True:
         print("-----Einkaufsliste-----")
         print("1. Artikel hinzufügen")
         print("2. Einkaufsliste anzeigen")
-        print("3. Programm beenden")
-        choice = input("1, 2 oder 3: ")
+        print("3. Artikel sortieren")
+        print("4. Artikel löschen")
+        print("5. Programm beenden")
+        choice = input("1, 2, 3, 4 oder 5: ")
         if choice == '1':
             add_item()
             continue
@@ -31,6 +48,12 @@ def menu():
             show_shoppinglist()
             continue
         elif choice == '3':
+            sorted_list()
+            continue
+        elif choice == '4':
+            del_item()
+            continue
+        elif choice == '5':
             break
         else:
             print("Falsche Eingabe!")
